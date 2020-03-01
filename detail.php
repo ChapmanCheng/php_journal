b41beaceee0a6f9f8e0bf9eeb19c15f4cab81022
chapmanCC <chapboy1989@gmail.com>
2020-03-02 02:11:09 +0800

    fix header.php

--- detail.php
+++ detail.php
@@ -12,72 +12,44 @@ if (isset($_GET['id'])) {
 } else {
     die('No journal entry selected');
 }
+
+require_once('./inc/header.php');
 ?>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>MyJournal</title>
-    <link href="https://fonts.googleapis.com/css?family=Cousine:400" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans:600" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/site.css">
-</head>
-
-<body>
-    <header>
-        <div class="container">
-            <div class="site-header">
-                <a class="logo" href="index.html"><i class="material-icons">library_books</i></a>
-                <a class="button icon-right" href="new.html"><span>New Entry</span> <i class="material-icons">add</i></a>
-            </div>
-        </div>
-    </header>
-    <section>
-        <div class="container">
-            <div class="entry-list single">
-                <article>
-                    <h1><?php echo $article['title']; ?></h1>
-                    <time datetime="<?php echo $article['date']; ?>"><?php echo dateString($article['date']); ?></time>
+<section>
+    <div class="container">
+        <div class="entry-list single">
+            <article>
+                <h1><?php echo $article['title']; ?></h1>
+                <time datetime="<?php echo $article['date']; ?>"><?php echo dateString($article['date']); ?></time>
+                <div class="entry">
+                    <h3>Time Spent: </h3>
+                    <p><?php echo $article['time_spent']; ?></p>
+                </div>
+                <div class="entry">
+                    <h3>What I Learned:</h3>
+                    <p><?php echo $article['learned']; ?></p>
+                </div>
+                <?php
+                if (!empty($article['resources'])) { ?>
                     <div class="entry">
-                        <h3>Time Spent: </h3>
-                        <p><?php echo $article['time_spent']; ?></p>
-                    </div>
-                    <div class="entry">
-                        <h3>What I Learned:</h3>
-                        <p><?php echo $article['learned']; ?></p>
-                    </div>
-                    <?php
-                    if (!empty($article['resources'])) { ?>
-                        <div class="entry">
-                            <h3>Resources to Remember:</h3>
-                            <ul>
-                                <?php
-                                foreach ($article['resources'] as $re)
-                                    echo "<li><a href=''>$re</a></li>";
-                                ?>
-                                <!-- <li><a href="">Cras accumsan cursus ante, non dapibus tempor</a></li>
+                        <h3>Resources to Remember:</h3>
+                        <ul>
+                            <?php
+                            foreach ($article['resources'] as $re)
+                                echo "<li><a href=''>$re</a></li>";
+                            ?>
+                            <!-- <li><a href="">Cras accumsan cursus ante, non dapibus tempor</a></li>
                             <li>Nunc ut rhoncus felis, vel tincidunt neque</li>
                             <li><a href="">Ipsum dolor sit amet</a></li> -->
-                            </ul>
-                        </div>
-                    <?php }; ?>
-                </article>
-            </div>
+                        </ul>
+                    </div>
+                <?php }; ?>
+            </article>
         </div>
-        <div class="edit">
-            <p><a href="edit.php?id=<?php echo $article['id']; ?>">Edit Entry</a></p>
-        </div>
-    </section>
-    <footer>
-        <div>
-            &copy; MyJournal
-        </div>
-    </footer>
-</body>
-
-</html>
+    </div>
+    <div class="edit">
+        <p><a href="edit.php?id=<?php echo $article['id']; ?>">Edit Entry</a></p>
+    </div>
+</section>
+<?php require_once('./inc/footer.php');
--- index.php
+++ index.php
@@ -5,52 +5,25 @@ require_once('./inc/db.php');
 $stmt = $db->query('select * from entries');
 $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+require_once('./inc/header.php');
 ?>
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>MyJournal</title>
-    <link href="https://fonts.googleapis.com/css?family=Cousine:400" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans:600" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/site.css">
-</head>
-
-<body>
-    <header>
-        <div class="container">
-            <div class="site-header">
-                <a class="logo" href="index.html"><i class="material-icons">library_books</i></a>
-                <a class="button icon-right" href="new.html"><span>New Entry</span> <i class="material-icons">add</i></a>
-            </div>
-        </div>
-    </header>
-    <section>
-        <div class="container">
-            <div class="entry-list">
-                <?php
-                foreach ($articles as $a) {
-                    $dateString = dateString($a['date']);
-                    echo "
+<section>
+    <div class="container">
+        <div class="entry-list">
+            <?php
+            foreach ($articles as $a) {
+                $dateString = dateString($a['date']);
+                echo "
                         <article>
                             <h2><a href='detail.php?id=$a[id]'>$a[title]</a></h2>
                             <time datetime='$a[date]'>$dateString</time>
                         </article>";
-                }
-                ?>
+            }
+            ?>
 
-            </div>
         </div>
-    </section>
-    <footer>
-        <div>
-            &copy; MyJournal
-        </div>
-    </footer>
-</body>
-
-</html>
+    </div>
+</section>
+<?php require_once('./inc/footer.php');
