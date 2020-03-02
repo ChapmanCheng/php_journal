d14a606e85fc45abc9f87ff7258686a1688b7d8c
Chapman Cheng <chapman@itwakes.me>
2020-03-02 17:00:12 +0800

    complete edit post retrieve data

--- edit.php
+++ edit.php
@@ -3,45 +3,63 @@
 require('./inc/class/Journal.php');
 require('./inc/functions.php');
 require_once('./inc/db.php');
-
 $formURL =  $_SERVER['PHP_SELF'];
 
+$j = new Journal();
 
 if (isset($_GET['id'])) {
     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-    // update database
     $formURL .= "?id=$id";
-} else {
-    if (!empty($_POST)) {
-        // create new entry
-        $journal = new Journal($db);
-        if ($journal->createNewEntry())
-            header('Location: ' . $_SERVER['SERVER_NAME']);
-    }
 
-    // if ($journal->checkExist(['title', 'date', 'time_spent', 'learned'])) {
-    /**
-     * vardump
-     * object(Journal)#2 (5) { 
-     *  ["title"]=> string(6) "foobar" 
-     *  ["date"]=> string(10) "2020-03-03" 
-     *  ["time_spent"]=> string(7) "3 hours" 
-     *  ["learn"]=> string(7) "nothing" 
-     *  ["resources"]=> string(0) "" }
-     */
+    // get data from database
+    $sql = 'SELECT * FROM entries where id = :id';
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
 
-    // if ($journal->checkExist(['title', 'date', 'time_spent', 'learned'])) {
-    //     $sql = 'INSERT INTO entries (title, date, time_spent, learned, resources)
-    //             VALUES (:title, :date, :time_spent, :learned, :resources)';
-    //     $stmt = $db->prepare($sql);
-    //     $stmt->execute($journal->data);
-    // $stmt->bindParam(':title', $data['title'], PDO::PARAM_STR);
-    // $stmt->bindParam(':date', $data['date'], PDO::PARAM_STR);
-    // $stmt->bindParam(':time_spent', $data['time_spent'], PDO::PARAM_STR);
-    // $stmt->bindParam(':learnd', $data['learned'], PDO::PARAM_STR);
-    // $stmt->bindParam(':resources', $data['resources'], PDO::PARAM_STR);
-    // }
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    $j->insertData($data);
 }
+// } else {
+
+// if (Journal::hasPostData()) {
+// create new entry
+//     $journal = new Journal();
+//     $journal->retrievePostData();
+//     $journal->validateDate();
+//     $journal->linkDatabase($db);
+
+//     try {
+//         $journal->createNewEntry();
+//         header('Location: ' . '/php_journal');
+//     } catch (Exception $e) {
+//         die($e->getMessage());
+//     }
+// }
+
+// if ($journal->checkExist(['title', 'date', 'time_spent', 'learned'])) {
+/**
+ * vardump
+ * object(Journal)#2 (5) { 
+ *  ["title"]=> string(6) "foobar" 
+ *  ["date"]=> string(10) "2020-03-03" 
+ *  ["time_spent"]=> string(7) "3 hours" 
+ *  ["learn"]=> string(7) "nothing" 
+ *  ["resources"]=> string(0) "" }
+ */
+
+// if ($journal->checkExist(['title', 'date', 'time_spent', 'learned'])) {
+//     $sql = 'INSERT INTO entries (title, date, time_spent, learned, resources)
+//             VALUES (:title, :date, :time_spent, :learned, :resources)';
+//     $stmt = $db->prepare($sql);
+//     $stmt->execute($journal->data);
+// $stmt->bindParam(':title', $data['title'], PDO::PARAM_STR);
+// $stmt->bindParam(':date', $data['date'], PDO::PARAM_STR);
+// $stmt->bindParam(':time_spent', $data['time_spent'], PDO::PARAM_STR);
+// $stmt->bindParam(':learnd', $data['learned'], PDO::PARAM_STR);
+// $stmt->bindParam(':resources', $data['resources'], PDO::PARAM_STR);
+// }
+// }
 require_once('./inc/header.php');
 ?>
 <section>
@@ -53,23 +71,24 @@ require_once('./inc/header.php');
             <form method="POST" action="<?php echo $formURL; ?>">
 
                 <label for="title"> Title</label>
-                <input id="title" type="text" name="title" <?php if (isset($journal)) echo "value='$journal->title'"; ?>><br>
+                <input id="title" type="text" name="title" <?php if (isset($j)) echo "value='$j->title'"; ?>><br>
 
                 <label for="date">Date</label>
-                <input id="date" type="date" name="date" <?php if (isset($journal)) echo "value='$journal->date'"; ?>><br>
+                <input id="date" type="date" name="date" <?php if (isset($j)) echo "value='$j->date'"; ?>><br>
 
                 <label for="time-spent"> Time Spent</label>
-                <input id="time-spent" type="text" name="time_spent" <?php if (isset($journal)) echo "value='$journal->time_spent'"; ?>><br>
+                <input id="time-spent" type="text" name="time_spent" <?php if (isset($j)) echo "value='$j->time_spent'"; ?>><br>
 
                 <label for="what-i-learned">What I Learned</label>
-                <textarea id="what-i-learned" rows="5" name="learned"><?php if (isset($journal)) echo $journal->learned; ?></textarea>
+                <textarea id="what-i-learned" rows="5" name="learned"><?php if (isset($j)) echo $j->learned; ?></textarea>
 
                 <label for="resources-to-remember">Resources to Remember</label>
-                <textarea id="resources-to-remember" rows="5" name="resources"><?php if (isset($journal)) echo $journal->resources; ?></textarea>
+                <textarea id="resources-to-remember" rows="5" name="resources"><?php if (isset($j)) echo $j->resources; ?></textarea>
 
                 <input type="submit" value="Publish Entry" class="button">
 
                 <a href="#" class="button button-secondary">Cancel</a>
+
             </form>
         </div>
     </div>
--- inc/class/Journal.php
+++ inc/class/Journal.php
@@ -1,52 +1,97 @@
 <?php
 class Journal
 {
-    // private $title;
-    // private $date;
-    // private $time_spent;
-    // private $learn;
-    // private $resources;
-    private $data = array();
-    private $db;
+    private $title;
+    private $date;
+    private $time_spent;
+    private $learned;
+    private $resources;
 
-    public function __construct($db)
+    public function __construct()
     {
-        $this->db = $db;
-        foreach ($_POST as $key => $value) {
-            $this->data[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_STRING);
-        }
+        if (!empty($_POST))
+            foreach ($_POST as $key => $value)
+                $this->{$key} = filter_input(INPUT_POST, $key, FILTER_SANITIZE_STRING);
     }
+
     public function __get($name)
     {
-        if ($name == 'data') {
-            return $this->data;
-        } elseif (array_key_exists($name, $this->data)) {
-            $data = $this->data[$name];
-            return $data;
+        if (property_exists($this, $name)) {
+            return $this->{$name};
         }
         return false;
     }
-    public function checkExist($keys)
+
+    public function __set($name, $value)
     {
-        foreach ($keys as $key) {
-            if (empty($this->data[$key])) {
-                echo $key . ' is empty';
-                return false;
-            };
+        if (property_exists($this, $name)) {
+            $this->{$name} = $value;
+        } else {
+            return false;
         }
-        return true;
     }
+
+
+    public function insertData($asso_array)
+    {
+        foreach ($asso_array as $key => $val)
+            if (property_exists($this, $key))
+                $this->{$key} = $val;
+    }
+
+    /**
+     * test if object's properties are empty
+     * @param {$suppliedKeys} Optional. array of properties to test if respective properties are empty
+     * @return if empty, returns true, else false
+     */
+    public function isEmpty($suppliedKeys = null)
+    {
+        $vars = get_object_vars($this);
+
+        foreach ($vars as $key => $val)
+            if (
+                is_null($suppliedKeys) // default 
+                || in_array($key, $suppliedKeys) // if include in key criteria
+            )
+                if (empty($val))
+                    return true;
+        return false;
+    }
+
+    /**
+     * check if $this->date is a validate date and convert to database accepted format
+     * @return {boolean} check date is true or wrong
+     */
+    public function validateDate()
+    {
+        if (isset($this->data['date'])) {
+            try {
+                $date = date('Y-m-d', strtotime($this->data['date']));
+                $this->data['date'] = $date;
+                return true;
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Print date into sentence format e.g. June 06, 1989
+     * @return {string} date string
+     */
     public function printDateString()
     {
         $timestamp = strtotime($this->time_spent);
         return date('F d, Y', $timestamp);
     }
-    public function createNewEntry()
+
+    /**
+     * Convert object's properties to associative array
+     * @return {array} returns an asso array of properties
+     */
+    public function getAssoArrayProps()
     {
-        if ($this->checkExist(['title', 'date', 'time_spent', 'learned'])) {
-            $sql = 'INSERT INTO entries (title, date, time_spent, learned, resources)
-                    VALUES (:title, :date, :time_spent, :learned, :resources)';
-            return $this->db->prepare($sql)->execute($this->data);
-        }
+        return get_object_vars($this);
     }
 }
